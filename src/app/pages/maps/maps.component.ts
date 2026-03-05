7e98de3d5965e89be592dceba309b307038d9eeb
Erika Viveros <viverozerika@gmail.com>
2026-03-04 20:26:37 -0600

    Cambios visuales

--- src/app/pages/maps/maps.component.ts
+++ src/app/pages/maps/maps.component.ts
@@ -1,4 +1,5 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 declare var google: any;
 
@@ -9,23 +10,45 @@ declare var google: any;
 })
 
 export class MapsComponent implements OnInit {
+
+    map: any;
+
+    constructor(private http: HttpClient) { }
+
     ngOnInit() {
-        var myLatlng = new google.maps.LatLng(40.748817, -73.985428);
-        var mapOptions = {
-          zoom: 13,
-          center: myLatlng,
-          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-          styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
+        const cdmx = new google.maps.LatLng(19.4326, -99.1332);
 
-        }
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+        const mapOptions = {
+            zoom: 12,
+            center: cdmx,
+            scrollwheel: false
+        };
 
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            title:"Hello World!"
-        });
+        this.map = new google.maps.Map(
+            document.getElementById("map"),
+            mapOptions
+        );
 
-        // To add the marker to the map, call setMap();
-        marker.setMap(map);
+        this.loadWifiPoints();
+    }
+
+    loadWifiPoints() {
+        this.http.get<any[]>('assets/data/wifi.json')
+            .subscribe(data => {
+
+                data.forEach(point => {
+
+                    const marker = new google.maps.Marker({
+                        position: {
+                            lat: parseFloat(point.latitud),
+                            lng: parseFloat(point.longitud)
+                        },
+                        map: this.map,
+                        title: point.id
+                    });
+
+                });
+
+            });
     }
 }
